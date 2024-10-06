e4d33581ab40d1305d575143e234838797dcc1ec
peshkash17 <kalpeshpawar17680@gmail.com>
2024-10-06 15:42:02 +0530

    new project added

--- README.md
+++
@@ -1,2 +0,0 @@
-??portfolio
-https://github.com/user-attachments/assets/2508d181-5b05-409e-92f7-fca49ea5dfdf
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -86,6 +86,38 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a href="https://github.com/peshkash17/kalpesh-linkedin-ai-reply" target="\_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" />
+                </a>
+               
+              </div>
+            </header>
+            <div className="body">
+              <h3>LinkedIn AI Assistant Chrome Extension</h3>
+              <p>
+              Developed a Chrome extension using WXT (Web Extension Framework) to streamline the development process with modern tooling.Created an AI-powered messaging assistant that helps users generate contextual responses on LinkedIn conversations.Leveraged WXT's build system and TypeScript support to implement robust extension architecture with React components.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>WXT</li>
+                <li>React Js</li>
+                <li>TypeScript</li>
+                <li>Tailwind CSS</li>
+                <li>Chrome Extension Manifest V3</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
         {/* <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
